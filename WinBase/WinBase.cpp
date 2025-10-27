2a01794adb4cdc759d68a524ad76c803d5b8981d
George <936728057@qq.com>
2025-10-27 14:58:16 +0800

    Win32开发：各种字符编码格式

--- WinBase/WinBase.cpp
+++ WinBase/WinBase.cpp
@@ -10,6 +10,12 @@
   6.消息循环Message Loop，从消息队列中获取消息并分发给窗口过程函数WndProc处理
   7.各种消息处理
 */
+/*
+  系统调用函数的参数类型：
+  LPSTR==char*；LPCSTR==const char*；
+  LPWSTR==wchar_t*；LPCWSTR==const wchar_t*；
+  LPCTSTR==TCHAR*；LPCTSTR==const TCHAR*；
+*/
 
 /*
   2.窗口过程函数WndProc，处理窗口的各种消息
@@ -20,6 +26,8 @@ LRESULT CALLBACK WndProc(HWND hWnd, UINT msgID, WPARAM wParam, LPARAM lParam)
 }
 /*
   1.定义WinMain函数，作为Windows应用程序的入口点
+  句柄：Windows操作系统中用于标识各种对象内存位置的一个唯一值，但绝不是指针（三个句柄例外）
+  1.标准输入句柄 2.标准输出句柄 3.标准错误句柄；相当于Linux中的文件描述符0（标准输入设备）、1（标准输出设备）、2（标准错误设备）
   HINSTANCE实例句柄；HWND窗口句柄
 */
 int CALLBACK WinMain(_In_ HINSTANCE hIns,	//当前程序的实例句柄
@@ -36,13 +44,17 @@ int CALLBACK WinMain(_In_ HINSTANCE hIns,	//当前程序的实例句柄
 	wc.hIcon = NULL;		//使用系统默认图标
 	wc.hInstance = hIns;	//当前应用程序实例句柄
 	wc.lpfnWndProc = WndProc;	//窗口过程函数
-	wc.lpszClassName = TEXT("Main");	//窗口类名
+	//宽字节字符wchar_t每个字符占2个字节，窄字节字符char每个字符占1个字节
+	//定义宽字节字符串时，需要在字符串前加L，例如L"Main"
+	wc.lpszClassName = L"Main";	//窗口类名
 	wc.lpszMenuName = NULL;	//窗口菜单名
 	wc.style = CS_HREDRAW | CS_VREDRAW;	//窗口风格
 	RegisterClass(&wc);	//将以上赋值全部写入操作系统
 
 	//4.创建窗口CreateWindow，在内存中创建窗口
-	HWND hWnd = CreateWindow(TEXT("Main"), TEXT("window"), WS_OVERLAPPEDWINDOW, 100, 100, 500, 500, NULL, NULL, hIns, NULL);
+	//TEXT宏根据#ifdef  UNICODE，来决定创建宽字节字符窗口还是窄字节字符窗口
+	//L"Main" = TEXT("Main") =  __TEXT("Main") =（取决于是否定义了UNICODE）"Main"
+	HWND hWnd = CreateWindow(TEXT("Main"), __TEXT("window"), WS_OVERLAPPEDWINDOW, 100, 100, 500, 500, NULL, NULL, hIns, NULL);
 
 	//5.显示窗口ShowWindow，将内存中的窗口显示到屏幕上
 	ShowWindow(hWnd, SW_SHOW);	//显示窗口
---
+++ WinChar/WinChar.cpp
@@ -0,0 +1,49 @@
+﻿#include <Windows.h>
+#include <stdio.h>
+
+void C_char()
+{
+	const char* pszText = "Hello Char";
+	int len = strlen(pszText);
+	printf("%s %d\n", pszText, len);
+}
+
+void W_char()
+{
+	const wchar_t* pszText = L"Hello WChar";
+	int len = wcslen(pszText);
+	wprintf(L"%s %d\n", pszText, len);
+}
+
+void T_char()
+{
+	const TCHAR* pszText = TEXT("Hello TChar");
+#ifdef UNICODE
+	int len = wcslen(pszText);
+	wprintf(L"%s %d\n", pszText, len);
+#else
+	int len = strlen(pszText);
+	printf("%s %d\n", pszText, len);
+#endif // !UNICODE
+}
+
+void PrintUnicode()
+{
+	wchar_t unicodeStr[] = L"你好，世界"; // "Hello, World" in Chinese
+	//可以验证wprintf()函数对Unicode中文字符输出的支持不完善
+	wprintf(L"%ls\n", unicodeStr);		//中文输出变为?????
+	//Windows下推荐使用WriteConsole宏函数输出Unicode字符
+	//WriteConsole(标准输出句柄,unicodeStr,wcslen(unicodeStr),NULL);
+	//GetStdHandle(STD_INPUT_HANDLE)获取标准输入句柄；GetStdHandle(STD_OUTPUT_HANDLE)获取标准输出句柄；GetStdHandle(STD_ERROR_HANDLE)获取标准错误句柄
+	HANDLE hConsole = GetStdHandle(STD_OUTPUT_HANDLE);
+	WriteConsole(hConsole, unicodeStr, wcslen(unicodeStr), NULL, NULL);
+}
+
+int main()
+{
+	C_char();
+	W_char();
+	T_char();
+	PrintUnicode();
+	return 0;
+}
