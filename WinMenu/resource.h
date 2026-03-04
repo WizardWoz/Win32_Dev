11b6efc257dc2424bddfe5bc1369f3199a15f7d5
George <936728057@qq.com>
2026-03-04 15:47:07 +0800

    优化了项目目录

--- WinMenu/resource.h
+++ WinMenu/resource.h
@@ -15,15 +15,14 @@
 #define ID_EXIT                         40006
 #define ID_40007                        40007
 #define ID_ABOUT                        40008
-#define ID_ACCELERATOR40011             40011
 #define ID_SET                          40011
 
 // Next default values for new objects
 // 
 #ifdef APSTUDIO_INVOKED
 #ifndef APSTUDIO_READONLY_SYMBOLS
-#define _APS_NEXT_RESOURCE_VALUE        111
-#define _APS_NEXT_COMMAND_VALUE         40013
+#define _APS_NEXT_RESOURCE_VALUE        114
+#define _APS_NEXT_COMMAND_VALUE         40015
 #define _APS_NEXT_CONTROL_VALUE         1001
 #define _APS_NEXT_SYMED_VALUE           101
 #endif
